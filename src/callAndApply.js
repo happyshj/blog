aad2b43d901c705efc6b08ea2e2e13836caca093
sunhejuan <sunhejuan@renrenche.com>
2019-01-03 14:35:09 +0800

    模拟call和apply优化

--- src/callAndApply.js
+++ src/callAndApply.js
@@ -22,22 +22,39 @@ function bar(name, age) {
  * 3. 删除该函数
  */
 Function.prototype.call2 = function(context) {
-    const args = [...arguments].slice(1)
-    let c = context || window
-    c.fn = this
-    const result = c.fn(...args)
-    delete c.fn
+    const self = this
+    let obj
+    if (context === null || context === undefined) {
+        obj = window
+    } else {
+        obj = Object(context)
+    }
+    const args = [...arguments].slice(1) // 后面的参数
+    let result
+    obj.fn = self
+    result = obj.fn(...args)
+    delete obj.fn
     return result
 }
 
 // console.log(bar.call2(foo, 'kevin', 18))
 
 Function.prototype.apply2 = function(context) {
-    const args = arguments[1]
-    let c = context || window
-    c.fn = this
-    const result = c.fn(...args)
-    delete c.fn
+    const self = this // 调用apply的函数
+    // 设置apply修改的this
+    let obj
+    if (context === null || context === undefined) {
+        obj = window
+    } else {
+        obj = Object(context)
+    }
+    // 传递的参数
+    const args = arguments[1] || []
+
+    // 模拟apply
+    obj.fn = self
+    const result = obj.fn(...args)
+    delete obj.fn
     return result
 }
 

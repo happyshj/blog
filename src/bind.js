68e6b80833823761a08887b1f77254c28a3c100c
sunhejuan <sunhejuan@renrenche.com>
2019-01-03 15:09:04 +0800

    模拟bind优化

--- src/bind.js
+++ src/bind.js
@@ -14,24 +14,47 @@ function bar(name, age) {
 
 bar.prototype.friend = 'kevin'
 
-Function.prototype.bind2 = function (context) {
+/**
+ * 模拟bind函数的实现
+ * 最后是return一个函数，this值改变，并且有参数
+ * bind函数还可以作为构造函数，这时候忽略this
+ * 使用instanceof判断是否是构造函数
+ */
+Function.prototype.bind2 = function(context) {
     if (typeof this !== 'function') {
         throw new Error('Function.prototype.bind - what is trying to be bound is not callable')
     }
 
-    const self = this
+    const self = this // 相当于bar方法
+    // 设置this
+    let obj
+    if (context === null || context === undefined) {
+        obj = window
+    } else {
+        obj = Object(context)
+    }
+    // 设置第一次传递的参数
     const args = [...arguments].slice(1)
 
-    const fNOP = function () {}
+    // 设置空函数
+    const funP = function () {}
 
-    const fBOund = function () {
+    // 返回绑定this后的方法
+    const bindF = function() {
+        const bindSelf = this
+        if (bindSelf instanceof bindF) {
+            // 说明是构造函数
+            obj = bindSelf
+        }
+        // 设置执行bind函数时传递的参数
         const bindArgs = [...arguments]
-        return self.apply(this instanceof fNOP ? this : context, args.concat(bindArgs))
+        // 借用call
+        const result = self.apply(obj, args.concat(bindArgs))
+        return result
     }
-
-    fNOP.prototype = self.prototype
-    fBOund.prototype = new fNOP()
-    return fBOund
+    funP.prototype = self.prototype
+    bindF.prototype = new funP()
+    return bindF
 }
 
 const bindFoo = bar.bind2(null, 'daisy')
